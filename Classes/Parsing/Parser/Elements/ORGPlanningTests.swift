05bfd9c5bf682d738b91e6291b6121457d7a1051
dive <artyom.loenko@mac.com>
2019-10-22 22:14:56 +0100

    added unit-tests for ORGPlanning

--- Classes/Parsing/Parser/Elements/ORGPlanningTests.swift
+++ Classes/Parsing/Parser/Elements/ORGPlanningTests.swift
@@ -3,31 +3,50 @@
 //  MobileOrgTests
 //
 //  Created by Artem Loenko on 22/10/2019.
-//  Copyright © 2019 Sean Escriva. All rights reserved.
+//  Copyright © 2019 Artem Loenko. All rights reserved.
 //
 
 import XCTest
+@testable import MobileOrg
 
 class ORGPlanningTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_thatMatchWorksForCorrectKeywords() {
+        let planning = ["DEADLINE: <2020-01-07 Tue> SCHEDULED: <2019-12-21 Sat>",
+                        "DEADLINE: <2020-01-07 Tue>",
+                        "SCHEDULED: <2019-12-21 Sat>",
+                        "SCHEDULED: <2019-10-23 Wed 08:00 ++1d>",
+                        "SCHEDULED: <2019-11-01 Fri ++1m>",
+                        "SCHEDULED: <2019-10-26 Sat ++7d>",
+                        "SCHEDULED: <2019-10-26 Sat +1y>",
+                        "SCHEDULED: <2019-10-23 Wed 08:00>"]
+        planning.forEach { XCTAssertTrue(ORGPlanning.match(string: $0)) }
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_thatMatchIgnoresInCorrectOrVerySimilarKeywords() {
+        let planning = ["DEADLINE: 2020-01-07 Tue SCHEDULED: 2019-12-21 Sat",
+                        "DEADLINE <2020-01-07 Tue>",
+                        "SCHEDULED::: <2019-12-21 Sat>",
+                        "SCHEDULED <2019/10/23 Wed 08:00 ++1d>",
+                        "SCHEDULED: <2019 11 01 Fri>",
+                        "SCHEDULED: <2019 May 27 Sat>",
+                        "SCHEDULED: <2019-10-26 12 +1y>",
+                        "ASSIGNED: <2019-10-23 Wed 08:00>"]
+        planning.forEach { XCTAssertFalse(ORGPlanning.match(string: $0)) }
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_thatParserProducesProperResultsForScheduled() {
+        let planning = "SCHEDULED: <2019-12-21 Sat 08:00 +1w>"
+        let sut = ORGPlanning(string: planning)
+        XCTAssertEqual(sut.type, "SCHEDULED")
+        XCTAssertEqual(sut.planned, "<2019-12-21 Sat 08:00 +1w>")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_thatParserProducesProperResultsForDeadline() {
+        let planning = "DEADLINE: <2019-12-21 Sat>"
+        let sut = ORGPlanning(string: planning)
+        XCTAssertEqual(sut.type, "DEADLINE")
+        XCTAssertEqual(sut.planned, "<2019-12-21 Sat>")
     }
 
 }
